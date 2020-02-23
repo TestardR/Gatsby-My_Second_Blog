b5d5c501aa875c4fc94433fc070deaccfcbe01c3
unknown <rmtestard@gmail.com>
2020-02-23 18:20:50 +0100

    I have implemented a projects list page and project page

--- gatsby/pagination/create-projects-pages.js
+++ gatsby/pagination/create-projects-pages.js
@@ -9,24 +9,24 @@ module.exports = async (graphql, actions) => {
   const result = await graphql(`
     {
       allMarkdownRemark(
-        filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } }
+        filter: { frontmatter: { template: { eq: "project" } } }
       ) { totalCount }
     }
   `);
   
   const { postsPerPage } = siteConfig;
-  const numPages = Math.ceil(result.data.allMarkdownRemark.totalCount / postsPerPage);
+  const numPages = Math.ceil(result.data.allMarkdownRemark.totalCount);
 
   for (let i = 0; i < numPages; i += 1) {
     createPage({
-      path: i === 0 ? '/' : `/page/${i}`,
-      component: path.resolve('./src/templates/index-template.js'),
+      path: i === 0 ? '/projects' : `/projects/${i}`,
+      component: path.resolve('./src/templates/project-list-template.js'),
       context: {
         currentPage: i,
         postsLimit: postsPerPage,
         postsOffset: i * postsPerPage,
-        prevPagePath: i <= 1 ? '/' : `/page/${i - 1}`,
-        nextPagePath: `/page/${i + 1}`,
+        prevPagePath: i <= 1 ? '/projects' : `/projects/${i - 1}`,
+        nextPagePath: `/projects/${i + 1}`,
         hasPrevPage: i !== 0,
         hasNextPage: i !== numPages - 1
       }
